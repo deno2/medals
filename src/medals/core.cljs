0629d0c3a592da10c8cd227f8de604b2307868df
Dene Simpson <dene@automationsquared.com>
2019-06-18 12:50:45 +0100

    created empty widget component and display it in home-page

--- src/medals/core.cljs
+++ src/medals/core.cljs
@@ -1,12 +1,27 @@
 (ns medals.core
-    (:require
-      [reagent.core :as r]))
+  (:require
+   [reagent.core :as r]))
 
 ;; -------------------------
 ;; Views
 
+
+(defn medals
+  ([]
+   (medals "gold"))
+  ([sort]
+   [:div.medals-widget
+    [:h3 "Medal Count"]]))
+
+
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  [:div [:h2 "Medals Widget Demo"]
+   [:div
+    [:p "Default widget with no parameters (defaults to sorted by gold medals):"]
+    [medals]]
+   [:div
+    [:p "Widget with initial sort set to 'gold':"]
+    [medals "gold"]]])
 
 ;; -------------------------
 ;; Initialize app
