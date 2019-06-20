28dbd90fb6148ef452c513f1f7c22f82ac659c5f
Dene Simpson <dene@automationsquared.com>
2019-06-20 17:06:53 +0100

    Added medals.core.widget js function

--- src/medals/core.cljs
+++ src/medals/core.cljs
@@ -5,8 +5,6 @@
 
 ;; -------------------------
 ;; Views
-
-
 (defn home-page []
   [:div [:h2 "Medals Widget Demo"]
    [:div
@@ -14,13 +12,28 @@
     [widget/medals]]
    [:div
     [:p "Widget with initial sort set to 'bronze':"]
-    [widget/medals "bronze"]]])
+    [widget/medals "bronze"]]
+   [:div
+    [:p "Widget created with JavaScript, initial sort set to 'silver' via the code:"]
+    [:code "<div id='medalElem'></div>" [:br]
+     "<script type='text/javascript'>" [:br]
+     "  medals.core.widget('medalElem', 'silver');" [:br]
+     "</script>"]
+    [:div#medalElem]]])
 
 ;; -------------------------
 ;; Initialize app
-
 (defn mount-root []
   (r/render [home-page] (.getElementById js/document "app")))
 
+
 (defn init! []
   (mount-root))
+
+
+(defn ^:export widget
+  "Will mount an instance of the Medals widget on the element with the given elementId"
+  ([elementId]
+   (widget elementId "gold"))
+  ([elementId sortKey]
+   (r/render [widget/medals sortKey] (.getElementById js/document elementId))))
